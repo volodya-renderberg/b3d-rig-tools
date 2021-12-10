3254ab2efadfe74dbcc14891599965afc57cdbe7
volodya-renderberg <volodya.renderberg@gmail.com>
2021-12-10 11:54:59 +0300

    ***

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@
 
 import bpy
 
-# from . import funcs as fn
+from . import working as w
 # from . import settings
 
 
@@ -18,13 +18,57 @@ class RIGTOOLS_PT_main_panel(bpy.types.Panel):
         col = layout.column(align = True)
         row = col.row(align = True)
         row.label(text='Tools')
+        row = col.row(align = True)
+        row.label(text='Position and Action:')
+        row.operator("rigtools.push_position_and_action", text="Push")
+        row.operator("rigtools.pull_position_and_action", text="Pull")
 
         
+class RIGTOOLS_push_position_and_action(bpy.types.Operator):
+    bl_idname = "rigtools.push_position_and_action"
+    bl_label = "Push position and action"
 
+    def execute(self, context):
+        # (1)
+        b,r=w.push_position_and_action(context)
+        if not b:
+            self.report({'WARNING'}, r)
+        else:
+            self.report({'INFO'}, r)
+        return{'FINISHED'}
+
+
+class RIGTOOLS_pull_position_and_action(bpy.types.Operator):
+    bl_idname = "rigtools.pull_position_and_action"
+    bl_label = "Pull position and action"
+
+    def execute(self, context):
+        # (1)
+        b,r=w.pull_position_and_action(context)
+        if not b:
+            self.report({'WARNING'}, r)
+        else:
+            self.report({'INFO'}, r)
+        return{'FINISHED'}
+
+
+### TEMPLATE
+class RIGTOOLS_template_operator(bpy.types.Operator):
+    bl_idname = "rigtools.template_operator"
+    bl_label = "Text"
+
+    def execute(self, context):
+        # (1)
+        self.report({'INFO'}, "template_operator")
+        return{'FINISHED'}
 
 def register():
     bpy.utils.register_class(RIGTOOLS_PT_main_panel)
+    bpy.utils.register_class(RIGTOOLS_push_position_and_action)
+    bpy.utils.register_class(RIGTOOLS_pull_position_and_action)
 
     
 def unregister():
     bpy.utils.unregister_class(RIGTOOLS_PT_main_panel)
+    bpy.utils.unregister_class(RIGTOOLS_push_position_and_action)
+    bpy.utils.unregister_class(RIGTOOLS_pull_position_and_action)
